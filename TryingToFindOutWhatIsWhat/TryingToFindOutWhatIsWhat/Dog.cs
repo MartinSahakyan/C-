89bcf5733feaa583eec86c84505420c819c1d64c
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-03 17:54:33 +0400

    Adding class and EventHandler

---
+++ TryingToFindOutWhatIsWhat/TryingToFindOutWhatIsWhat/Dog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingToFindOutWhatIsWhat
+{
+    public class Trainer
+    {
+        void Operate()
+        {
+            var dog = new Poodle();
+            dog.HasSpoken += dog_HasSpoken;
+        }
+
+        private void dog_HasSpoken(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public  partial class Dog
+    {
+       
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public event EventHandler HasSpoken; 
+        public void Speak(string what = "bark")
+        {
+            //TODO
+            if (HasSpoken!=null)
+                HasSpoken(this, EventArgs.Empty);
+        }
+        public void Speak(int times,bool sit= true,string what = "bark")
+        {
+            //TODO
+        }
+
+    }
+    class Poodle : Dog
+    {
+        void x()
+        {
+            this.Speak(2, sit:false);
+        }
+    }
+}
---
+++ TryingToFindOutWhatIsWhat/TryingToFindOutWhatIsWhat/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingToFindOutWhatIsWhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello Guys";
+            Console.WriteLine(message);
+            Console.WriteLine("Hello World");
+            
+            const int a = 10;
+            
+        }
+
+       
+    }
+    public  class MyClass
+    {
+
+        public string Myfield = string.Empty;
+        public MyClass()
+        {
+        }
+        public void MyMethod(int parameter1, int parameter2)
+        {
+            Console.WriteLine("FirstParameter{1},SecondParameter{2}", parameter1, parameter2);
+        }
+        public int MyAutoMakingProperty { get; set;}
+        private int MyPropertyVar;
+        public int MyProperty
+        {
+            get
+            {
+                return MyPropertyVar;
+            }
+            set { MyPropertyVar = value; }
+        }
+    }
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+}
