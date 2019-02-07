0f57f87218681b829898447585d649df253c914a
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-08 03:17:07 +0400

    learning debuging in loop

---
+++ ForItereation/ForIteration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+                if (i == 4)
+                {
+                    Console.WriteLine("Hey 4");
+                    break;
+                }
+            }
+        }
+    }
+}
