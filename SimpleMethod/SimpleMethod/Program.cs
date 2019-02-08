25f0209a8065d67ff23842b5bcb2f88f47a98bea
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-08 22:03:02 +0400

    The Hello Worldmethod

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Armenia();
+            HelloWorld();
+        }
+
+        private static void Armenia()
+        {
+            string drosh = "Կարմիր Կապույտ Ծիրանագույն";
+            string orhnerg = "Մեր Հայրենիք";
+            string zinanshan = "Առյուծ և Արծիվ";
+            Console.WriteLine("{0} {1} {2}", drosh, orhnerg, zinanshan);
+        }
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
