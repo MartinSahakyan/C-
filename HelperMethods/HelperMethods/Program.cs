5969683a2819e9265d48cf15133cf60589109f1f
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-09 00:40:57 +0400

    Without method

---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+            Console.WriteLine("What is Your FirstName");
+            string FirstName=Console.ReadLine();
+            Console.WriteLine("What is Your LastName");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Where were you born");
+            string Country = Console.ReadLine();
+            char[] firstNameArray = FirstName.ToCharArray();
+            Array.Reverse(firstNameArray);
+            char[] lastNameArray = LastName.ToCharArray();
+            Array.Reverse(lastNameArray);
+            char[] YourCountry = Country.ToCharArray();
+            Array.Reverse(YourCountry);
+            string result = "";
+            foreach (var item in firstNameArray)
+            {
+                result += item;
+            }
+            result += " ";
+            foreach (var item in lastNameArray)
+            {
+                result += item;
+            }
+            result += " ";
+            foreach (var item in YourCountry)
+            {
+                result += item;
+            }
+            result += " ";
+            Console.WriteLine(result);
+        }
+
+    }
+}
