d7e17bd0985bbc28ebf217cf3cc21a2b7dc98d1b
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-09 00:48:16 +0400

    With method

--- HelperMethods/HelperMethods/Program.cs
+++ HelperMethods/HelperMethods/Program.cs
@@ -17,29 +17,21 @@ namespace HelperMethods
             string LastName = Console.ReadLine();
             Console.WriteLine("Where were you born");
             string Country = Console.ReadLine();
-            char[] firstNameArray = FirstName.ToCharArray();
-            Array.Reverse(firstNameArray);
-            char[] lastNameArray = LastName.ToCharArray();
-            Array.Reverse(lastNameArray);
-            char[] YourCountry = Country.ToCharArray();
-            Array.Reverse(YourCountry);
+            ReverseString(FirstName);
+            ReverseString(LastName);
+            ReverseString(Country);
+        }
+        static private void ReverseString(string Sentence)
+        {
+            char[] sentence = Sentence.ToCharArray();
+            Array.Reverse(sentence);
             string result = "";
-            foreach (var item in firstNameArray)
+            foreach (var item in sentence)
             {
                 result += item;
             }
             result += " ";
-            foreach (var item in lastNameArray)
-            {
-                result += item;
-            }
-            result += " ";
-            foreach (var item in YourCountry)
-            {
-                result += item;
-            }
-            result += " ";
-            Console.WriteLine(result);
+            Console.Write(result);
         }
 
     }
