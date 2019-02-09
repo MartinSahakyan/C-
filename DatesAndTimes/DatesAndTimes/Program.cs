4ed3801dd6dcb6e0f240415526699a1735e02f57
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-10 02:22:57 +0400

    playing with date and time

---
+++ DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToOADate());
+            //Console.WriteLine(myValue.ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            DateTime MyBirthday = new DateTime(1999, 03, 24);
+            //Console.WriteLine(MyBirthday.ToLongDateString()); 
+            TimeSpan myAge = DateTime.Now.Subtract(MyBirthday);
+            Console.WriteLine(myAge.TotalDays);
+        }
+    }
+}
