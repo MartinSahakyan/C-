25f91130f73fbe4d0d9d48afcc8582eb7c92dcd8
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-10 18:51:03 +0400

    2nd project of Euler

---
+++ EulerProject2/EulerProject2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 1, second = 1, delta, Sum = 0;
+            while (first <= 4000000 )
+            {
+                if (first%2==0)
+                {
+                    Sum += first;
+                }
+                delta = first;
+                first = second + first;
+                second = delta;
+            }
+            Console.WriteLine("Sum is = {0}", Sum);
+        }
+    }
+}
