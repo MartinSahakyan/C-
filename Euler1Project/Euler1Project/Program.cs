d8e6bbe00b801217c2f241085d3f8725392ce02d
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-10 16:33:54 +0400

    1st project of Euler

---
+++ Euler1Project/Euler1Project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler1Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SumOfAllTheMultiples = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    SumOfAllTheMultiples = SumOfAllTheMultiples + i;
+                }
+            }
+            Console.WriteLine("Sum = {0}",SumOfAllTheMultiples);
+        }
+    }
+}
