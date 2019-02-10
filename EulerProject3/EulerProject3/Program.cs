074819da28b692f7872e251d39d6e27cdd0a4fd6
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-11 00:05:37 +0400

    3nd project of Euler

---
+++ EulerProject3/EulerProject3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject3
+{
+    class Program
+    {
+        static void Main()
+        {
+            long Number = 600851475143;
+            for (long i = 0; i < 600851475143 / 2; i++)
+            {
+                bool prime = PrimeTool.IsPrime(i);
+                if (prime)
+                {
+                    if (Number % i == 0)
+                    {
+                        Console.WriteLine("prime factor {0}",i);
+                        Number = Number / i;
+                    }
+                }
+            }
+        }
+    }
+}
