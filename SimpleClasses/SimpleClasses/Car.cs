f03d8722099e4471508379eb2786c233c8622556
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-10 15:53:58 +0400

    starting working with classes

---
+++ SimpleClasses/SimpleClasses/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public decimal DetermineMarketValue()
+        {
+            decimal CarValue;
+            if (Year > 1990)
+                CarValue = 10000;
+            else
+                CarValue = 100;
+            return CarValue;
+        }    
+    }
+}
---
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ferrari = new Car();
+            Ferrari.Color = "Red";
+            Ferrari.Model = "Monza Spi";
+            Ferrari.Year = 1999;
+            Console.WriteLine("{0},{1},{2}", Ferrari.Year, Ferrari.Model, Ferrari.Color);
+            decimal value = DetermineMarketValue(Ferrari);
+            Console.WriteLine("{0:C}", value);
+            Console.WriteLine("{0:C}", Ferrari.DetermineMarketValue());
+            Console.ReadLine();
+            
+        }
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
+
+        }
+    }
+}
