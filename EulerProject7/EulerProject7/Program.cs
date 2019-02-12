fdf1ee71818bb7944397c54d029bee11a629ecc5
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-12 23:28:37 +0400

    7th Project of Euler

---
+++ EulerProject7/EulerProject7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (PrimeTool.IsPrime(i))
+                {
+                    index++;
+                }
+                if (index == 10001)
+                {
+                    Console.WriteLine("The 10001st prime number is {0}",i);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ EulerProject7/EulerProject7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EulerProject7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EulerProject7")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9569eb9-800f-4240-819f-dae2d3a8af80")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
