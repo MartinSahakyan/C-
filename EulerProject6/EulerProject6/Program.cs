cfd5c51c9ca4d55e6d44157c60e4d6e5e0391783
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-12 22:36:05 +0400

    6th Project of Euler

---
+++ EulerProject6/EulerProject6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Please enter your number = ");
+            number = Int32.Parse(Console.ReadLine());
+
+            SquareOfSum(number);
+            SumOfSquares(number);
+            int Result = SquareOfSum(number) - SumOfSquares(number);
+            Console.WriteLine("Your SquareOfSum - SumOfSquares = {0} ",Result);
+        }
+        public static int SumOfSquares(int n)
+        {
+            int Sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Sum = Sum + i * i;
+            }
+            return Sum;
+        }
+        public static int SquareOfSum( int n)
+        {
+            int Sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Sum = Sum + i;
+            }
+            int product = Sum * Sum;
+            return product;
+        }
+    }
+}
