8e5a744d013a9a486f79418acb04e7deb16a2a78
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-15 02:48:16 +0400

    10th project of Euler

---
+++ EulerProject10/EulerProject10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long Number = 2000000;
+            long Sum = 0;
+            for (long i = 0; i < Number; i++)
+            {
+                bool prime = PrimeTool.IsPrime(i);
+                if (prime)
+                {
+                    Sum = Sum + i;
+                }
+
+            }
+            Console.WriteLine("Sum is = {0}", Sum);
+        }
+    }
+}
