627d06219b3d6370c829dbb61b46099a9713d2f0
MartinSahakyan <martinsahakyan3@gmail.com>
2019-02-15 02:57:58 +0400

    more effective way

--- EulerProject10/EulerProject10/Program.cs
+++ EulerProject10/EulerProject10/Program.cs
@@ -10,18 +10,30 @@ namespace EulerProject10
     {
         static void Main(string[] args)
         {
-            long Number = 2000000;
-            long Sum = 0;
-            for (long i = 0; i < Number; i++)
+            int Number = 2000000;
+            int Sum = 0;
+            for (int i = 0; i < Number; i++)
             {
-                bool prime = PrimeTool.IsPrime(i);
-                if (prime)
+                
+                if (IsPrime(i))
                 {
                     Sum = Sum + i;
                 }
 
             }
             Console.WriteLine("Sum is = {0}", Sum);
+            
+        }
+        public static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            if (number % 2 == 0) return (number == 2);
+            int root = (int)Math.Sqrt((double)number);
+            for (int i = 3; i <= root; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
         }
     }
 }
