03b42a54b272c0e3c0c88e966c5ef4703156e94e
MartinSahakyan <martinsahakyan3@gmail.com>
2019-03-05 02:04:14 +0400

    Des algorythms with forms

---
+++ Des/Des/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+namespace Des
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Encrypt()
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] key = ASCIIEncoding.ASCII.GetBytes("asxcvfds");
+            byte[] iv = ASCIIEncoding.ASCII.GetBytes("cxxcdkkk");
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(key, iv), CryptoStreamMode.Write);
+            StreamWriter sw = new StreamWriter(cs);
+            sw.Write(textBox1.Text);
+            sw.Flush();
+            cs.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
+        private string Decrypt()
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] key = ASCIIEncoding.ASCII.GetBytes("asxcvfds");
+            byte[] iv = ASCIIEncoding.ASCII.GetBytes("cxxcdkkk");
+            MemoryStream ms = new MemoryStream(Convert.FromBase64String(textBox2.Text));
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(key, iv), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cs);
+            return sr.ReadToEnd();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Encrypt();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Decrypt();
+        }
+
+        
+    }
+}
---
+++ TrippleDES/TrippleDES/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace TrippleDES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider tDes = new TripleDESCryptoServiceProvider();
+            tDes.Key = md5.ComputeHash(utf8.GetBytes(textBox1.Text));
+            tDes.Mode = CipherMode.ECB;
+            tDes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform trans = tDes.CreateEncryptor();
+            textBox3.Text = BitConverter.ToString(trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length));
+        }
+    }
+}
